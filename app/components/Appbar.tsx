79dc26e98178ed90f71b8b7a85d8588dfb3941e9
prabesh bhandari <prabeshbhandari842@gmail.com>
2024-10-17 22:52:55 +0545

    fix: make responsive header

--- app/components/Appbar.tsx
+++ app/components/Appbar.tsx
@@ -1,35 +1,65 @@
 import { useSession } from "next-auth/react"
+import { useState } from "react"
 import { Logout } from "./Logout"
 import { Login } from "./Login"
 import { GoToDashboard } from "./GoToDashboard"
+import { Menu, X } from "lucide-react"
 
 const Appbar = () => {
     const session = useSession()
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen)
+    }
+
     return (
-    <header>
-    <nav className="border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-900">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="/" className="flex items-center">
-                <img className="mr-3 h-6 sm:h-9" src="https://flowbite.com/docs/images/logo.svg" alt="Testimonial Logo" />
-                <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Testimonial</span>
-            </a>
-            <div className="flex items-center lg:order-2">
-                <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
-                    <span className="sr-only">Open main menu</span>
-                    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-                    <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                </button>
-            </div>
-            <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                    {!session.data?.user && <Login />}
-                    {session.data?.user && <Logout />}
-                    {session.data?.user && <GoToDashboard />}
-                </ul>
-            </div>
-        </div>
-    </nav>
-    </header>
+        <header>
+            <nav className="border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-900">
+                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                    <a href="/" className="flex items-center">
+                        <img 
+                            className="mr-3 h-6 sm:h-9" 
+                            src="https://flowbite.com/docs/images/logo.svg" 
+                            alt="Testimonial Logo" 
+                        />
+                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                            Testimonial
+                        </span>
+                    </a>
+                    
+                    {/* Mobile menu button */}
+                    <button 
+                        onClick={toggleMobileMenu}
+                        className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        aria-controls="mobile-menu-2"
+                        aria-expanded={isMobileMenuOpen}
+                    >
+                        <span className="sr-only">Toggle mobile menu</span>
+                        {isMobileMenuOpen ? (
+                            <X className="w-6 h-6" />
+                        ) : (
+                            <Menu className="w-6 h-6" />
+                        )}
+                    </button>
+
+                    {/* Navigation menu - desktop and mobile */}
+                    <div className={`${
+                        isMobileMenuOpen ? 'block' : 'hidden'
+                    } w-full lg:block lg:w-auto lg:order-1`}>
+                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                            {!session.data?.user && <Login />}
+                            {session.data?.user && (
+                                <>
+                                    <Logout />
+                                    <GoToDashboard />
+                                </>
+                            )}
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
     )
 }
 
