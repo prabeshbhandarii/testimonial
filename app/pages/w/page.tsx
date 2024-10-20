e0fa11afe4881453043eff9f021108ea88e4cf39
prabesh bhandari <prabeshbhandari842@gmail.com>
2024-10-20 23:41:34 +0545

    test

--- app/pages/w/page.tsx
+++ app/pages/w/page.tsx
@@ -13,12 +13,6 @@ interface SpaceData{
 export default function Page(){
     const url = usePathname()
     const spaceName = url.split('/')[2]
-    const [spaceData, setSpaceData] = useState<SpaceData>({
-        id: '',
-        name: '',
-        description: '',
-        message: ''
-      });
     return(
         <ScriptDisplay spaceName={spaceName} />
     )
