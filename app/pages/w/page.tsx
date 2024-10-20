38b555e11905896f6bd08b3c950a2e4bbb8e9bf7
prabesh bhandari <prabeshbhandari842@gmail.com>
2024-10-20 23:46:02 +0545

    remove unused variables

--- app/pages/w/page.tsx
+++ app/pages/w/page.tsx
@@ -1,14 +1,6 @@
 "use client"
 import ScriptDisplay from "@/app/components/ScriptDisplay";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
-
-interface SpaceData{
-    id: string,
-    name: string, 
-    description: string, 
-    message: string;
-  }
 
 export default function Page(){
     const url = usePathname()
