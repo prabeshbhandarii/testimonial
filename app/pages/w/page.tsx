40021e126769aab7b85d079ff84767b5a88e4c3d
prabesh bhandari <prabeshbhandari842@gmail.com>
2024-10-20 23:37:33 +0545

    test

--- app/pages/w/page.tsx
+++ app/pages/w/page.tsx
@@ -1,7 +1,25 @@
+"use client"
 import ScriptDisplay from "@/app/components/ScriptDisplay";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+
+interface SpaceData{
+    id: string,
+    name: string, 
+    description: string, 
+    message: string;
+  }
 
 export default function Page(){
+    const url = usePathname()
+    const spaceName = url.split('/')[2]
+    const [spaceData, setSpaceData] = useState<SpaceData>({
+        id: '',
+        name: '',
+        description: '',
+        message: ''
+      });
     return(
-        <ScriptDisplay spaceName={'bestcake'} />
+        <ScriptDisplay spaceName={spaceName} />
     )
 }
